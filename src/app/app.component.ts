bd58ac11659e48a600b7b77d841fa7d40206c745
Laurent Goudet <me@laurentgoudet.com>
2021-01-11 11:06:09 +0100

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
+    <!-- ngTemplateOutlet is mispelled below -->
     <ng-container *ngTemplateOutle="myTemplate"></ng-container>
     <ng-template #myTemplate>
       My template is here
